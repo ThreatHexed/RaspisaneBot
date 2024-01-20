55bd8d7a1a38824bb24d269b7707db65f882565f
ThreatHexed <98428296+ThreatHexed@users.noreply.github.com>
2024-01-20 17:26:14 +0300

    PZIDAAAAA

---
+++ bot_start.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+import bot_config
+from bot_handlers import router
+
+class BOT():
+    
+    async def main(self):
+        dp = Dispatcher(storage=MemoryStorage())
+        bot = Bot(token=bot_config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+        dp.include_router(router)
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+        
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    p = BOT()
+    asyncio.run(p.main())
